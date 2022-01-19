d6eb086be2d0d23ff8b562b30e1623342d0e1a4e
Ankit gray <grayankit@gmail.com>
2022-01-19 20:09:27 +0530

    First Changes

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to technicalankit
+        </p>
+        <p className='testtext'>
+          Subscribe
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://technicalankit.netlify.app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Technical Ankit
         </a>
       </header>
     </div>
