1951089b8b87abca23460c7d4d613cd407d48677
Ankit gray <grayankit@gmail.com>
2022-01-25 11:43:04 +0530

    React Router dom added navigation added and about page added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 import Navbar from './components/Navbar.js';
 import Textarea from './components/Textarea';
 import Alert from './components/Alert';
-// import About from './components/About';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
@@ -33,11 +38,20 @@ function App() {
   }
   return (
     <>
-    <Navbar title="Textutils" toggleMode={toggleMode} mode={mode}/>
-    <Alert alert={alert}/>
-    <div className="container">
-    <Textarea heading="Enter your text below to analyze" toggleMode={toggleMode} mode={mode} showAlert={showAlert}/>
-    </div>
+    <Router>
+      <Navbar title="Textutils" toggleMode={toggleMode} mode={mode}/>
+      <Alert alert={alert}/>
+      <div className="container">
+        <Routes>
+          {/* // work of exact is to route to exact path of directory */}
+      {/* /users --> Component 1
+      /users/home --> Component 2 */}
+          <Route exact path="/about" element={<About mode={mode}/>}/>
+          <Route exact path="/" element={<Textarea showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>}/>
+            {/* <Textarea showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/> */}
+        </Routes>
+      </div>
+    </Router>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,32 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..."/>
+
+        <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="/" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+      <div className="card" aria-hidden="true">
+        <img src="..." className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title placeholder-glow">
+          <span className="placeholder col-6"></span>
+          </h5>
+          <p className="card-text placeholder-glow">
+          <span className="placeholder col-7"></span>
+          <span className="placeholder col-4"></span>
+          <span className="placeholder col-4"></span>
+          <span className="placeholder col-6"></span>
+          <span className="placeholder col-8"></span>
+          </p>
+          <a href="/" tabIndex="-1" className="btn btn-primary disabled placeholder col-6">hello</a>
+        </div>
+      </div>
     </div>
   )
 }
