aba1fecd1095ea85bfbd58a4ed60b4a8490aab8f
Ankit gray <grayankit@gmail.com>
2022-01-25 17:17:53 +0530

    About page changed

--- src/components/About.js
+++ src/components/About.js
@@ -1,32 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function About() {
+export default function About(props) {
   return (
     <div>
-      <div className="card">
-        <img src="..." className="card-img-top" alt="..."/>
+      <div className={`card bg-${props.mode}`} style={{color:props.mode==='dark'?'white':"#042743"}}>
+        <img src="../../public/github.svg" className="card-img-top" alt="..."/>
 
         <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="/" className="btn btn-primary">Go somewhere</a>
+        <h5 className="card-title">Free to use</h5>
+        <p className="card-text">This Textutil app is free to use and the code of this website is availabel to opensource</p>
+        <a href="/" className="btn btn-primary">Source Code</a>
         </div>
       </div>
 
-      <div className="card" aria-hidden="true">
+      <div className={`card mx-2 my-2 bg-${props.mode}`} style={{color: props.mode==='dark'?'white':'#042743'}}>
         <img src="..." className="card-img-top" alt="..."/>
         <div className="card-body">
-          <h5 className="card-title placeholder-glow">
-          <span className="placeholder col-6"></span>
+          <h5 className="card-title">
+            Edit you text in a easy and quick way
+          {/* <span className="placeholder col-6"></span> */}
           </h5>
-          <p className="card-text placeholder-glow">
-          <span className="placeholder col-7"></span>
-          <span className="placeholder col-4"></span>
-          <span className="placeholder col-4"></span>
-          <span className="placeholder col-6"></span>
-          <span className="placeholder col-8"></span>
+          <p className="card-text">
+          <span>Convert to <strong>Uppercase</strong></span>
           </p>
-          <a href="/" tabIndex="-1" className="btn btn-primary disabled placeholder col-6">hello</a>
+          <p className="card-text">
+          <span>Convert to <strong>Lowercase</strong></span>
+          </p>
+          <p className="card-text">
+          <span><strong>Copy </strong>your text to clipboard</span>
+          </p>
+          <Link to="/" tabIndex="-1" className="btn btn-primary">Home</Link>
         </div>
       </div>
     </div>
